bf9e28f5a4b33a2938ea0a411779b776d7a711e9
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-09-01 12:48:15 +0700

    Update snippets

--- snippets.js
+++ snippets.js
@@ -1,14 +1,16 @@
 var data = [];
 for (var i in warriors) {
     for (var j = 0; j < warriors[i].length; j++) {
-		for (var k = 0; k < warriors[i][j].Stats.length; k++) {
-			var arr = warriors[i][j].Stats[k].split(': ');
-			if (arr[0] == 'Attack' && warriors[i][j].Game == 'SW2') {
-				data.push({
-					'Name': i,
-					'Weapon': warriors[i][j].Name,
-					'Stat': warriors[i][j].Stats[k]
-				});
+		if (warriors[i][j].Game == 'SW2') {
+			for (var k = 0; k < warriors[i][j].Stats.length; k++) {
+				var arr = warriors[i][j].Stats[k].split(': ');
+				if (arr[0] == 'Attack') {
+					data.push({
+						'Name': i,
+						'Weapon': warriors[i][j].Name,
+						'Stat': warriors[i][j].Stats[k]
+					});
+				}
 			}
 		}
 	}
