6f106d7273df613644d41d3519b82d0de96e2089
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-09-15 14:26:20 +0700

    Update Yoshihiro guide

--- scripts/constants.js
+++ scripts/constants.js
@@ -1221,7 +1221,7 @@ const warriors = {
 				"Range: 35"
 			],
 			'Stage': "Conquest for Kyushu",
-			'Requirement': "Within 7 minutes succeed in the the missions to defeat Nobuchika Chosokabe, defeat Hidehisa Sengoku, defeat Ekei Ankokuji, defeat Ginchiyo Tachibana and kill 300 enemies."
+			'Requirement': "Within 7 minutes succeed in the the missions to defeat Nobuchika Chosokabe, defeat Hidehisa Sengoku, defeat Ekei Ankokuji, defeat Ginchiyo Tachibana and defeat 300 enemies."
 		}
 	],
 	'Ginchiyo': [
